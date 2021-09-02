2baf0f004760bfb4555d7f65962903ea22c4dbc3
Nguyễn Phước Cảnh Hiếu <89906382+NguyenPhuocCanhHieu@users.noreply.github.com>
2021-09-02 18:46:47 +0700

    baitap2

---
+++ btap2/welcome.java
@@ -0,0 +1,6 @@
+// This program prints welcome to Java!
+public class welcome {
+	public static void main(String[] args) {
+		System.out.println("Welcome to Java!");
+	}
+}
--- welcome.java
+++
@@ -1,6 +0,0 @@
-// This application program prints welcome to Java!
-public class welcome {
-	public static void main(String[] avgs) {
-		System.out.println("Welcome to Java!");
-	}
-}
