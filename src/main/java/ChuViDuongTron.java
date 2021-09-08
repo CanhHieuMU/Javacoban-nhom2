adb4282e559355ad7fd8838e76f0ef97a0a22d79
Nguyễn Phước Cảnh Hiếu <89906382+NguyenPhuocCanhHieu@users.noreply.github.com>
2021-09-08 15:18:04 +0700

    Init Project

---
+++ src/main/java/ChuViDuongTron.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class ChuViDuongTron {
+    public static void main(String[] args) {
+        double r;
+        final double PI = 3.14519;
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Nhap Ban Kinh Duong Tron :");
+        r = scanner.nextDouble();
+        double C = r*2*PI;
+        System.out.println("Chu Vi Duong Tron La: "+C);       
+    }
+}
+    
